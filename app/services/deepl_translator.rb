4bc5fa7bb35439d96635f1181f2dd465f5f8e5f8
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-11-08 13:55:45 +0100

    Fix rubocop offenses

--- app/services/deepl_translator.rb
+++ app/services/deepl_translator.rb
@@ -24,15 +24,13 @@ class DeeplTranslator
   end
 
   def deepl_translation(text, source_locale, target_locale)
-    begin
-      DeepL.translate text, source_locale, target_locale
-    rescue DeepL::Exceptions::RequestError => e
-      Rails.logger.debug '--- An error occurred on Deepl translation ---'
-      Rails.logger.debug "Deepl HTTP response code: #{e.response.code}"
-      Rails.logger.debug "Deepl HTTP response body: #{e.response.body}"
-      Rails.logger.debug "Deepl HTTP Request body: #{e.request.body}"
-      Rails.logger.debug "---------------------------------------------"
-      ""
-    end
+    DeepL.translate text, source_locale, target_locale
+  rescue DeepL::Exceptions::RequestError => e
+    Rails.logger.debug "--- An error occurred on Deepl translation ---"
+    Rails.logger.debug "Deepl HTTP response code: #{e.response.code}"
+    Rails.logger.debug "Deepl HTTP response body: #{e.response.body}"
+    Rails.logger.debug "Deepl HTTP Request body: #{e.request.body}"
+    Rails.logger.debug "---------------------------------------------"
+    ""
   end
 end
